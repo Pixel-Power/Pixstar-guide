12c9bb300c3068c4d63eabeacd0374cc39f14929
김진용 <jyjy0509@naver.com>
2024-03-24 20:51:02 +0900

    feature/Login/secondDraft

--- src/Pages/Login.js
+++ src/Pages/Login.js
@@ -1,75 +1,76 @@
 import React, { useState, useEffect } from 'react';
-import { useParams } from 'react-router-dom';
-import { getUserDetail } from '../apis/MemAPI';
+import usersData from '../data/memDetail.json'; 
 
+const Login = () => {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [id, setId] = useState('');
+  const [password, setPassword] = useState('');
+  const [users, setUsers] = useState([]);
+  
+ 
 
+  useEffect(() => {
+    setUsers(usersData); 
+  }, []);
 
-function LoginPage() {
-    const [id, setId] = useState('');
-    const [password, setPassword] = useState('');
-    const [isLoggedIn, setIsLoggedIn] = useState(false);
-    const { userCode } = useParams();
+  
+  const handleLogin = () => {
+    let userFound = false;
+    for (let i = 0; i < users.length; i++) {
+      const user = users[i];
+      if (user.userId === id && user.userPw === password) {
+        userFound = true;
+        break;
+      }
+    }
+    if (userFound) {
+      setIsLoggedIn(true);
+      alert('로그인 되었습니다')
+    } else {
+      alert('아이디 또는 비밀번호가 잘못되었습니다');
+    }
+  };
 
-    const [user, setUser] = useState({
-        userName : '', 
-        userId : '',
-        userPw : '',
-        userEmail : '',
-        userPhone : ''
-    });
+  // 로그아웃 처리 함수
+  const handleLogout = () => {
+    setIsLoggedIn(false);
+    localStorage.removeItem('isLoggedIn');
+  };
 
-    useEffect(
-        ()=>{
-            setUser(getUserDetail(userCode));
-        },
-        [userCode]
-    )
+  return (
+    <div>
+      {isLoggedIn ? (
+        <div>
+          <p>반갑습니다, {id}님!</p>
+          <button onClick={handleLogout}>Logout</button>
+        </div>
+      ) : (
+        <div style={{
+            display: 'flex', justifyContent: 'center', alignItems: 'center',
+            width: '100%', height: '100vh'
+        }}>
+           <form style={{ display: 'flex', flexDirection: 'column' }}>
+           <label>ID</label>
+          <input
+            type="text"
+            placeholder="아이디를 입력해주세요"
+            value={id}
+            onChange={(e) => setId(e.target.value)}
+          />
+          <label>PASSWORD</label>
+          <input
+            type="password"
+            placeholder="비밀번호를 입력해주세요"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <button onClick={handleLogin}>로그인</button>
+          </form>
+        </div>
+      )}
+    </div>
+  );
+};
 
-   
-    const onIdHandler = (event) => {
-        setId(event.currentTarget.value);
-    };
+export default Login;
 
-    const onPasswordHandler = (event) => {
-        setPassword(event.currentTarget.value);
-    };
-
-    const onLoginHandler = () => {
-        if (user && (user.userId) === id && (user.userPw) === password) {
-            setIsLoggedIn(true);
-        } else {
-            alert("로그인에 실패했습니다.");
-        }
-        
-    };
-
-    useEffect(() => {
-        if (isLoggedIn) {
-            alert("로그인이 완료되었습니다.");
-        }
-    }, [isLoggedIn]);
-
-    return (
-        <>
-            <h1>로그인</h1>
-            <div style={{
-                display: 'flex', justifyContent: 'center', alignItems: 'center',
-                width: '100%', height: '100vh'
-            }}>
-                <form style={{ display: 'flex', flexDirection: 'column' }}>
-                    <label>ID</label>
-                    <input type="id" placeholder="아이디를 입력해주세요" value={id} onChange={onIdHandler} />
-                    <label>PASSWORD</label>
-                    <input type="password" placeholder="비밀번호를 입력해주세요" value={password} onChange={onPasswordHandler} />
-
-                    <br />
-                    <button type="button" onClick={onLoginHandler} >
-                        Login
-                    </button>
-                </form>
-            </div>
-        </>
-    );
-}
-
-export default LoginPage;
