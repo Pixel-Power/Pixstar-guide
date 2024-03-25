74b71cdc924804b4ecaffa0a2ec42a5ae378c92a
SOOBIN\tifkr <soobinnunu1101@gmail.com>
2024-03-25 15:37:09 +0900

    0325 검색결과 조회 페이지 수정

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import RestaurantSearchResult from "./Pages/Restaurant-search-result";
 import Login from './Pages/Login';
 import SignUp from './Pages/SignUp';
 import MyPage from './Pages/MyPage';
-// import RestaurantDetail from './Pages/Restaurant-detail';
+import RestaurantDetail from './Pages/Restaurant-detail';
 
 function App() {
   return (
@@ -23,7 +23,7 @@ function App() {
 
           <Route path="/" element={<Layout3/>}>
             <Route path="/login" element={<Login/>}/>
-            {/* <Route path="/restaurantdetail" element={<RestaurantDetail/>}/> */}
+            <Route path="/restaurantdetail" element={<RestaurantDetail/>}/>
           </Route>
         </Routes>
       </BrowserRouter>
--- src/Pages/Restaurant-search-result.js
+++ src/Pages/Restaurant-search-result.js
@@ -3,125 +3,63 @@ import {useState, useEffect} from 'react';
 import { searchRestaurant } from '../apis/RestaurantAPI';
 import ResDetailStyle from './Restaurant-search-result.module.css';
 
+
 function RestaurantSearchDetail(){
     const [restaurantList, setRestaurantList ] = useState([]);
     const [searchParams] = useSearchParams();
+    const itemsPerPage = 6;
+    const [currentPage, setCurrentPage] = useState(1);
 
     const  category = searchParams.get('category');
 
     useEffect(
         () => {
-            setRestaurantList()
+            setRestaurantList(searchRestaurant(category));
         },[category]
     );
+
+    const indexOfLastRestaurant = currentPage * itemsPerPage;
+    const indexOfFirstRestaurant = indexOfLastRestaurant - itemsPerPage;
+    const currentItems = restaurantList.slice(indexOfFirstRestaurant, indexOfLastRestaurant);
+
+    const paginate = pageNumber => setCurrentPage(pageNumber);
 
     return(
         <>
             <div className={ResDetailStyle.flexcontainer}>
             <div className={ResDetailStyle.ResTitleName}>{category} 맛집 검색 결과</div>
                 <div className={ResDetailStyle.flexItem1}>
-                    {restaurantList.length > 0 ? (
-                        restaurantList.map((restaurant, index) => (
+                    {currentItems.length > 0 ? (
+                        currentItems.map((restaurant, index) => (
                     <div key={index} className={ResDetailStyle.RestaurantBox1}>
                         <div className={ResDetailStyle.ResTitle}>
-                            <img src={restaurant.img} alt="식당" className={ResDetailStyle.img16}></img><br/>
-                            <img src="/img/3star.png" alt="픽스타"></img>
-                            <div className={ResDetailStyle.ResName}>{restaurant.name}</div>
+                            <img src={restaurant.img} alt="식당" className={ResDetailStyle.imgRes}></img><br/>
+                            <img src={restaurant.pixstar} className={ResDetailStyle.pixstar} alt="픽스타"></img><br/>
+                            <span className={ResDetailStyle.ResName}>{restaurant.name}</span>
                             <span className={ResDetailStyle.ResCategory}>#{restaurant.category}</span>
                         </div>
-                        <div className='ResDetail'>
-                            <img src="/img/location.png" alt='장소'></img>{restaurant.address}<br/>
-                            <img src="/img/phone.png" alt='전화'></img>{restaurant.phone}
-                            <div className='description'>재료의 선도가 좋기로 유명한, 서울에서 손꼽히는 일식집 중 하나다.<br/>
-                                            담백하고 정갈한 90여 가지의 정통 관서 지방의 요리를 선보인다.</div>
+                        <div className={ResDetailStyle.ResDetail}>
+                            <img src="images/restaurant-detail/location-pin.png" alt='장소' className={ResDetailStyle.address}></img>{restaurant.address}<br/>
+                            <img src="images/restaurant-detail/phone.png" alt='전화' className={ResDetailStyle.phone}></img>{restaurant.phone}
+                            <div className={ResDetailStyle.description}>{restaurant.description}</div>
                         </div>
                     </div>
+
                     ))
                     ) : (
-                        <div className={ResDetailStyle.noResult}>검색 결과가 없습니다.</div>
+                        <div className={ResDetailStyle.noResult}> </div>
                     )}
-                    <div className={ResDetailStyle.RestaurantBox2}>
-                        <div className={ResDetailStyle.ResTitle}>
-                            <img src="/img/img16.jpg" alt="식당" className={ResDetailStyle.img16}></img><br/>
-                            <img src="/img/3star.png" alt="픽스타"></img>
-                            <div className={ResDetailStyle.ResName}>식당이름</div>
-                            <span className={ResDetailStyle.ResCategory}>#일식</span>
-                        </div>
-                        <div className='ResDetail'>
-                            <img src="/img/location.png" alt='장소'></img>장소<br/>
-                            <img src="/img/phone.png" alt='전화'></img>0507-1234-5678
-                            <div className='description'>재료의 선도가 좋기로 유명한, 서울에서 손꼽히는 일식집 중 하나다.<br/>
-                                            담백하고 정갈한 90여 가지의 정통 관서 지방의 요리를 선보인다.</div>
-                        </div>
-                    </div>
-                    <div className={ResDetailStyle.RestaurantBox3}>
-                        <div className={ResDetailStyle.ResTitle}>
-                            <img src="/img/img16.jpg" alt="식당" className={ResDetailStyle.img16}></img><br/>
-                            <img src="/img/3star.png" alt="픽스타"></img>
-                            <div className={ResDetailStyle.ResName}>식당이름</div>
-                            <span className={ResDetailStyle.ResCategory}>#일식</span>
-                        </div>
-                        <div className='ResDetail'>
-                            <img src="/img/location.png" alt='장소'></img>장소<br/>
-                            <img src="/img/phone.png" alt='전화'></img>0507-1234-5678
-                            <div className='description'>재료의 선도가 좋기로 유명한, 서울에서 손꼽히는 일식집 중 하나다.<br/>
-                                            담백하고 정갈한 90여 가지의 정통 관서 지방의 요리를 선보인다.</div>
-                        </div>
-                    </div>
                 </div>
-                <div className={ResDetailStyle.flexItem2}>
-                    <div className={ResDetailStyle.RestaurantBox4}>
-                        <div className={ResDetailStyle.ResTitle}>
-                            <img src="/img/img16.jpg" alt="식당" className={ResDetailStyle.img16}></img><br/>
-                            <img src="/img/3star.png" alt="픽스타"></img>
-                            <div className={ResDetailStyle.ResName}>식당이름</div>
-                            <span className={ResDetailStyle.ResCategory}>#일식</span>
-                        </div>
-                        <div className='ResDetail'>
-                            <img src="/img/location.png" alt='장소'></img>장소<br/>
-                            <img src="/img/phone.png" alt='전화'></img>0507-1234-5678
-                            <div className='description'>재료의 선도가 좋기로 유명한, 서울에서 손꼽히는 일식집 중 하나다.<br/>
-                                            담백하고 정갈한 90여 가지의 정통 관서 지방의 요리를 선보인다.</div>
-                        </div>
-                    </div>
-                    <div className={ResDetailStyle.RestaurantBox5}>
-                        <div className={ResDetailStyle.ResTitle}>
-                            <img src="/img/img16.jpg" alt="식당" className={ResDetailStyle.img16}></img><br/>
-                            <img src="/img/3star.png" alt="픽스타"></img>
-                            <div className={ResDetailStyle.ResName}>식당이름</div>
-                            <span className={ResDetailStyle.ResCategory}>#일식</span>
-                        </div>
-                        <div className='ResDetail'>
-                            <img src="/img/location.png" alt='장소'></img>장소<br/>
-                            <img src="/img/phone.png" alt='전화'></img>0507-1234-5678
-                            <div className='description'>재료의 선도가 좋기로 유명한, 서울에서 손꼽히는 일식집 중 하나다.<br/>
-                                            담백하고 정갈한 90여 가지의 정통 관서 지방의 요리를 선보인다.</div>
-                        </div>
-                    </div>
-                    <div className={ResDetailStyle.RestaurantBox6}>
-                        <div className={ResDetailStyle.ResTitle}>
-                            <img src="/img/img16.jpg" alt="식당" className={ResDetailStyle.img16}></img><br/>
-                            <img src="/img/3star.png" alt="픽스타"></img>
-                            <div className={ResDetailStyle.ResName}>식당이름</div>
-                            <span className={ResDetailStyle.ResCategory}>#일식</span>
-                        </div>
-                        <div className='ResDetail'>
-                            <img src="/img/location.png" alt='장소'></img>장소<br/>
-                            <img src="/img/phone.png" alt='전화'></img>0507-1234-5678
-                            <div className='description'>재료의 선도가 좋기로 유명한, 서울에서 손꼽히는 일식집 중 하나다.<br/>
-                                            담백하고 정갈한 90여 가지의 정통 관서 지방의 요리를 선보인다.</div>
-                        </div>
-                    </div>
+
+
                 </div>
                 <div className={ResDetailStyle.paging}>
-                <NavLink to='#' className={ResDetailStyle.select1}>&lt;</NavLink>
-                <NavLink to='#' className={ResDetailStyle.select2}>1</NavLink>
-                <NavLink to='#' className={ResDetailStyle.select2}>2</NavLink>
-                <NavLink to='#' className={ResDetailStyle.select2}>3</NavLink>
-                <NavLink to='#' className={ResDetailStyle.select2}>4</NavLink>
-                <NavLink to='#' className={ResDetailStyle.select2}>5</NavLink>
-                <NavLink to='#' className={ResDetailStyle.select1}>&gt;</NavLink>
-            </div>
+                <NavLink to='#' className={ResDetailStyle.select1} onClick={() => paginate(currentPage - 1)}>&lt;</NavLink>
+                {restaurantList.length > 0 &&
+                    Array.from({ length: Math.ceil(restaurantList.length / itemsPerPage) }, (_, index) => (
+                        <NavLink key={index} to='#' className={index + 1 === currentPage ? ResDetailStyle.select2 : ResDetailStyle.select2} onClick={() => paginate(index + 1)}>{index + 1}</NavLink>
+                    ))}
+                <NavLink to='#' className={ResDetailStyle.select1} onClick={() => paginate(currentPage + 1)}>&gt;</NavLink>
             </div>
             
         </>
