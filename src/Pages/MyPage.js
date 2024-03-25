7db7a7f8d32c0b2cc802c129013853b0294fc63a
Bibi0104 <goqlsdl0120@gmail.com>
2024-03-25 18:25:51 +0900

    MyPage, Reservation commit

--- src/Pages/MyPage.js
+++ src/Pages/MyPage.js
@@ -15,53 +15,55 @@ function MyPage(){
                 </Link>
             </div>
             <div>
-                <button className={styles.Button}>리뷰</button>
-                <button className={styles.Button}>예약</button>
+                <button className={styles.Button1}>리뷰</button>
+                <Link to="/reservation">
+                    <button className={styles.Button2}>예약</button>
+                </Link>
             </div>
             <div>
-                <div reviewBoxAll>
-                    
-                        <div className={styles.reviewBox}> 
-                            <img className={styles.reviewImgs} src="/images/food/hamburger/hamburger (1).jpg" alt='reviewImg1'/>
-                            <h5>햄버거 맛집</h5>
-                            <img src="/images/myPage-images/5star.png" alt='5star'/>
-                            <br/>
-                            치즈버거가 너무 잘생겼고 사장님이 맛있습니다. 인생 최고의 햄버거 맛집
-                        </div>
-                    
+                <div>
+                
+                    <div className={styles.reviewBox}> 
+                        <img className={styles.reviewImgs} src="/images/food/hamburger/hamburger (1).jpg" alt='reviewImg1'/>
+                        <h4>햄버거 맛집</h4>
+                        <img src="/images/myPage-images/5star.png" alt='5star'/>
+                        <br/>
+                        치즈버거가 너무 잘생겼고 사s장님이 맛있습니다. 인생 최고의 햄버거 맛집
+                    </div>
+                
                     <div className={styles.reviewBox}> 
                         <img className={styles.reviewImgs} src="/images/food/chinesefood/chinesefood (1).jpg" alt='reviewImg2'/>
-                        <h5>중식 맛집</h5>
+                        <h4>중식 맛집</h4>
                         <img src="/images/myPage-images/5star.png" alt='5star'/>
                         <br/>
                         음식이 너무 아름답고 사장님이 화끈합니다. 인생 최고의 중국 음식 맛집❤
                     </div>
                     <div className={styles.reviewBox}> 
                         <img className={styles.reviewImgs} src="/images/food/hamburger/hamburger (1).jpg" alt='reviewImg3'/>
-                        <h5>햄버거 맛집</h5>
+                        <h4>햄버거 맛집</h4>
                         <img src="/images/myPage-images/5star.png" alt='5star'/>
                         <br/>
                         치즈버거가 너무 잘생겼고 사장님이 맛있습니다. 인생 최고의 햄버거 맛집
                     </div>
                 </div>
-                <div reviewBoxAll>
+                <div>
                     <div className={styles.reviewBox}> 
                         <img className={styles.reviewImgs} src="/images/food/hamburger/hamburger (1).jpg" alt='reviewImg4'/>
-                        <h5>햄버거 맛집</h5>
+                        <h4>햄버거 맛집</h4>
                         <img src="/images/myPage-images/5star.png" alt='5star'/>
                         <br/>
                         치즈버거가 너무 잘생겼고 사장님이 맛있습니다. 인생 최고의 햄버거 맛집
                     </div>
                     <div className={styles.reviewBox}> 
                         <img className={styles.reviewImgs} src="/images/food/hamburger/hamburger (1).jpg" alt='reviewImg5'/>
-                        <h5>햄버거 맛집</h5>
+                        <h4>햄버거 맛집</h4>
                         <img src="/images/myPage-images/5star.png" alt='5star'/>
                         <br/>
                         치즈버거가 너무 잘생겼고 사장님이 맛있습니다. 인생 최고의 햄버거 맛집
                     </div>
                     <div className={styles.reviewBox}> 
                         <img className={styles.reviewImgs} src="/images/food/hamburger/hamburger (1).jpg" alt='reviewImg6'/>
-                        <h5>햄버거 맛집</h5>
+                        <h4>햄버거 맛집</h4>
                         <img src="/images/myPage-images/5star.png" alt='5star'/>
                         <br/>
                         치즈버거가 너무 잘생겼고 사장님이 맛있습니다. 인생 최고의 햄버거 맛집
