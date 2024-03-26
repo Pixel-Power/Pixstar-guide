818eecb54dd644fd925295c35c7cc53ff50200cb
SOOBIN\tifkr <soobinnunu1101@gmail.com>
2024-03-26 17:45:49 +0900

    0326 Sidebar / 레스토랑 검색 결과 페이지 / 이미지 추가 commit

--- src/components/Sidebar1.js
+++ src/components/Sidebar1.js
@@ -14,41 +14,24 @@ function Sidebar1() {
     return(
         <div class={a.l_navbar}>
         <nav class={a.nav}>
-            <div className={a.navBox}>
+            <div className={a.navBox} style={{textAlign: 'center'}}>
+            <img src='/images/header-images/pixstar-guide-logo.png' alt='픽스타로고' className={a.sidelogo} style={{width: '100px', height: 'auto', display:'block',margin: '20px auto'}}></img>
                 <div class={a.nav__list}>
-                    <button type='button' className={a.nav__linkActive} onClick={onClickHandler}>해시태그별</button>
+                    <label type='button' className={a.nav__linkActive} onClick={onClickHandler}>해시태그별</label>
                     <ul className={isOpen? `${a.nav__linkCollapse}${a.showNav}` : `${a.nav__linkCollapse}`}>
                         {isOpen && ( 
                             <>
-                            <li><a href='#' className={a.nav_name}>#분좋카</a></li>
-                            <li><a href='#' className={a.nav_name}>#오마카세</a></li>
-                            <li><a href='#' className={a.nav_name}>#회식</a></li>
-                            <li><a href='#' className={a.nav_name}>#갓성비</a></li>
-                            <li><a href='#' className={a.nav_name}>#맛도링</a></li>
+                            <li className={a.nav_list}><a href='#' className={a.nav_name}>#분좋카</a></li>
+                            <li className={a.nav_list}><a href='#' className={a.nav_name}>#오마카세</a></li>
+                            <li className={a.nav_list}><a href='#' className={a.nav_name}>#회식</a></li>
+                            <li className={a.nav_list}><a href='#' className={a.nav_name}>#갓성비</a></li>
+                            <li className={a.nav_list}><a href='#' className={a.nav_name}>#맛도링</a></li>
                             </>
                         )
                         }
                     </ul>
-
-                    <div href="#" class={a.nav__linkCollapse}>
-                        <ion-icon name="folder-outline" class={a.nav__icon}></ion-icon>
-                        <span class={a.nav_name}>가격별</span>
-
-                        <ion-icon name="chevron-down-outline" class={a.collapse__link}></ion-icon>
-
-                        <ul class={a.collapse__menu}>
-                            <a href="#" class={a.collapse__sublink}>Data</a>
-                            <a href="#" class={a.collapse__sublink}>Group</a>
-                            <a href="#" class={a.collapse__sublink}>Members</a>
-                        </ul>
-                    </div>
-
-                    <div href="#" class={a.nav__linkCollapse}>
-                        <ion-icon name="people-outline" class={a.nav__icon}></ion-icon>
-                        <span class={a.nav_name}>픽스타별</span>
-
-                        <ion-icon name="chevron-down-outline" class={a.collapse__link}></ion-icon>
-                    </div>
+                    <label type='button' className={a.nav__linkActive} onClick={onClickHandler}>가격별</label><br/>
+                    <label type='button' className={a.nav__linkActive} onClick={onClickHandler}>픽스타별</label>
                 </div>
             </div>
         </nav>
