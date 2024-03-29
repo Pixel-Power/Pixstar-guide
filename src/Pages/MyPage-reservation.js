1a0d654afd3786356fc1627fd3e44674f1aba88f
Bibi0104 <goqlsdl0120@gmail.com>
2024-03-29 10:07:19 +0900

    cancelmember 버튼 link 주소 변경

--- src/Pages/MyPage-reservation.js
+++ src/Pages/MyPage-reservation.js
@@ -32,7 +32,7 @@ function Reservation(){
                     <p className={styles.UserName}>{user.userName}</p>님
                     <p className={styles.UserIntroduce}>주요 활동 지역 :</p>
                 </div>
-                <Link to="/mypagememberout">
+                <Link to="/cancelmember">
                     <img src="/images/myPage-images/editImg.png" alt="UserEdit"/>
                 </Link>
             </div>
